81f467e460f0638bf05b23fc8a800911a0662adc
Kapcap4 <kapcap43@gmail.com>
2017-10-17 12:58:49 +0400

    firstTask

--- src/es/esy/kapcapx/Main.java
+++ src/es/esy/kapcapx/Main.java
@@ -1,8 +1,34 @@
 package es.esy.kapcapx;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int fistNumber;
+        int secondNumber;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        fistNumber = scanner.nextInt();
+        System.out.println("Введите второе число");
+        secondNumber = scanner.nextInt();
+
+        System.out.println("Сумма чисел " + "[" + (fistNumber + secondNumber) + "]");
+        System.out.println("Разность чисел " + "[" + (fistNumber - secondNumber) + "]");
+        System.out.println("Деление чисел " + "[" + (fistNumber / secondNumber) + "]");
+        System.out.println("Остаток от деления чисел " + "[" + (fistNumber % secondNumber) + "]");
+        System.out.println("Деление по модулю 2 " + "[" + ((fistNumber / secondNumber) % 2) + "]");
+        System.out.println("******************" + "Оператор ++var" + "******************");
+        System.out.println("Инкремент чисел ++firstNumber " + "[" + ++fistNumber + "]" + "[" + ++secondNumber + "]");
+        System.out.println("Декремент чисел --firstNumber " + "[" + --fistNumber + "]" + "[" + --secondNumber + "]");
+        System.out.println("******************" + "Оператор var++" + "******************");
+        System.out.println("Сейчас переменная firstNumber " + "[" + fistNumber + "]" + " а переменная secondNumber " + "[" + secondNumber + "]");
+        System.out.println("Оператор (firstNumber++)  " + "[" + fistNumber++ + "]" + "  (secondNumber++) " + "[" + secondNumber++ + "]");
+        System.out.println("Сейчас переменная firstNumber " + "[" + fistNumber + "]" + " а переменная secondNumber " + "[" + secondNumber + "]");
+        System.out.println("******************" + "Оператор x+=" + "******************");
+        System.out.println("Сейчас переменная firstNumber " + "[" + fistNumber + "]" + " а переменная secondNumber " + "[" + secondNumber + "]");
+        System.out.println("Оператор (x+=2)  " + "[" + (fistNumber+=2) + "]" + "  (secondNumber++) " + "[" + (secondNumber+=2) + "]");
+
+
     }
 }
