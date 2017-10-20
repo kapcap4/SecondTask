3defb4252d14a9aec70aa5ceb608c74cef034b45
Kapcap4 <kapcap43@gmail.com>
2017-10-20 19:25:38 +0400

    2 sub-task

--- src/es/esy/kapcapx/Main.java
+++ src/es/esy/kapcapx/Main.java
@@ -18,17 +18,39 @@ public class Main {
         System.out.println("Деление чисел " + "[" + (fistNumber / secondNumber) + "]");
         System.out.println("Остаток от деления чисел " + "[" + (fistNumber % secondNumber) + "]");
         System.out.println("Деление по модулю 2 " + "[" + ((fistNumber / secondNumber) % 2) + "]");
+        // Оператор ++x
         System.out.println("******************" + "Оператор ++var" + "******************");
         System.out.println("Инкремент чисел ++firstNumber " + "[" + ++fistNumber + "]" + "[" + ++secondNumber + "]");
         System.out.println("Декремент чисел --firstNumber " + "[" + --fistNumber + "]" + "[" + --secondNumber + "]");
+        // Оператор x++
         System.out.println("******************" + "Оператор var++" + "******************");
         System.out.println("Сейчас переменная firstNumber " + "[" + fistNumber + "]" + " а переменная secondNumber " + "[" + secondNumber + "]");
         System.out.println("Оператор (firstNumber++)  " + "[" + fistNumber++ + "]" + "  (secondNumber++) " + "[" + secondNumber++ + "]");
         System.out.println("Сейчас переменная firstNumber " + "[" + fistNumber + "]" + " а переменная secondNumber " + "[" + secondNumber + "]");
+        // Оператор x+=
         System.out.println("******************" + "Оператор x+=" + "******************");
         System.out.println("Сейчас переменная firstNumber " + "[" + fistNumber + "]" + " а переменная secondNumber " + "[" + secondNumber + "]");
         System.out.println("Оператор (x+=2)  " + "[" + (fistNumber+=2) + "]" + "  (secondNumber++) " + "[" + (secondNumber+=2) + "]");
-
+        // Оператор <<
+        System.out.println("******************" + "Оператор <<" + "******************");
+        System.out.println("Сейчас переменная firstNumber " + "[" + fistNumber + "]" + " а переменная secondNumber " + "[" + secondNumber + "]");
+        System.out.println("Оператор (<<)  " + "[" + (fistNumber << 1) + "]" + "  (<<) " + "[" + (secondNumber << 1) + "]");
+        // Оператор >>
+        System.out.println("******************" + "Оператор >>" + "******************");
+        System.out.println("Сейчас переменная firstNumber " + "[" + fistNumber + "]" + " а переменная secondNumber " + "[" + secondNumber + "]");
+        System.out.println("Оператор (>>)  " + "[" + (fistNumber >> 1) + "]" + "  (>>) " + "[" + (secondNumber >> 1) + "]");
+        // Оператор >>>
+        System.out.println("******************" + "Оператор >>>" + "******************");
+        System.out.println("Сейчас переменная firstNumber " + "[" + fistNumber + "]" + " а переменная secondNumber " + "[" + secondNumber + "]");
+        System.out.println("Оператор (>>>)  " + "[" + (fistNumber >> 1) + "]" + "  (>>>) " + "[" + (secondNumber >> 1) + "]");
+        // Integer обёртка << >>
+        System.out.println("******************" + "Обёртка Integer" + "******************");
+        System.out.println("Сейчас переменная firstNumber " + "[" + fistNumber + "]" + " а переменная secondNumber " + "[" + secondNumber + "]");
+        System.out.println("Оператор (<<)  " + "[" + Integer.toBinaryString(Integer.rotateLeft(fistNumber, 1)) + "]" + "  (>>) " + "[" + Integer.toBinaryString(Integer.rotateRight(secondNumber, 1)) + "]");
+        // Оператор ~
+        System.out.println("******************" + "Оператор ~ Побитовый переворот числа" + "******************");
+        System.out.println("Сейчас переменная firstNumber " + "[" + fistNumber + "]" + " а переменная secondNumber " + "[" + secondNumber + "]");
+        System.out.println("Оператор (~)  " + "[" + (~fistNumber) + "]" + "  (~) " + "[" + (~secondNumber) + "]");
 
     }
 }
